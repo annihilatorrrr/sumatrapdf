b68517f4e375c1fe5659c22b06f437228c485656
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2026-02-12 04:09:45 +0100

    update cmd/trans-gen.ts

--- cmd/trans-gen.ts
+++ cmd/trans-gen.ts
@@ -12,24 +12,12 @@ const gLangs: string[][] = [
   ["bg", "Bulgarian (Български)", "_LANGID(LANG_BULGARIAN)"],
   ["bn", "Bengali (বাংলা)", "_LANGID(LANG_BENGALI)"],
   ["co", "Corsican (Corsu)", "_LANGID(LANG_CORSICAN)"],
-  [
-    "br",
-    "Portuguese - Brazil (Português)",
-    "MAKELANGID(LANG_PORTUGUESE, SUBLANG_PORTUGUESE_BRAZILIAN)",
-  ],
-  [
-    "bs",
-    "Bosnian (Bosanski)",
-    "MAKELANGID(LANG_BOSNIAN, SUBLANG_BOSNIAN_BOSNIA_HERZEGOVINA_LATIN)",
-  ],
+  ["br", "Portuguese - Brazil (Português)", "MAKELANGID(LANG_PORTUGUESE, SUBLANG_PORTUGUESE_BRAZILIAN)"],
+  ["bs", "Bosnian (Bosanski)", "MAKELANGID(LANG_BOSNIAN, SUBLANG_BOSNIAN_BOSNIA_HERZEGOVINA_LATIN)"],
   ["by", "Belarusian (Беларуская)", "_LANGID(LANG_BELARUSIAN)"],
   ["ca", "Catalan (Català)", "_LANGID(LANG_CATALAN)"],
   ["ca-xv", "Catalan-Valencian (Català-Valencià)", "(LANGID)-1"],
-  [
-    "cn",
-    "Chinese Simplified (简体中文)",
-    "MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED)",
-  ],
+  ["cn", "Chinese Simplified (简体中文)", "MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED)"],
   ["cy", "Welsh (Cymraeg)", "_LANGID(LANG_WELSH)"],
   ["cz", "Czech (Čeština)", "_LANGID(LANG_CZECH)"],
   ["de", "German (Deutsch)", "_LANGID(LANG_GERMAN)"],
@@ -42,6 +30,7 @@ const gLangs: string[][] = [
   ["fa", "Persian (فارسی)", "_LANGID(LANG_FARSI)", "RTL"],
   ["fi", "Finnish (Suomi)", "_LANGID(LANG_FINNISH)"],
   ["fr", "French (Français)", "_LANGID(LANG_FRENCH)"],
+  ["fo", "Faroese (Føroyskt)", "_LANGID(LANG_FAEROESE)"],
   ["fy-nl", "Frisian (Frysk)", "_LANGID(LANG_FRISIAN)"],
   ["ga", "Irish (Gaeilge)", "_LANGID(LANG_IRISH)"],
   ["gl", "Galician (Galego)", "_LANGID(LANG_GALICIAN)"],
@@ -55,12 +44,7 @@ const gLangs: string[][] = [
   ["jv", "Javanese (ꦧꦱꦗꦮ)", "(LANGID)-1"],
   ["ka", "Georgian (ქართული)", "_LANGID(LANG_GEORGIAN)"],
   ["kr", "Korean (한국어)", "_LANGID(LANG_KOREAN)"],
-  [
-    "ku",
-    "Kurdish (كوردی)",
-    "MAKELANGID(LANG_CENTRAL_KURDISH, SUBLANG_CENTRAL_KURDISH_CENTRAL_KURDISH_IRAQ)",
-    "RTL",
-  ],
+  ["ku", "Kurdish (كوردی)", "MAKELANGID(LANG_CENTRAL_KURDISH, SUBLANG_CENTRAL_KURDISH_CENTRAL_KURDISH_IRAQ)", "RTL"],
   ["kw", "Cornish (Kernewek)", "(LANGID)-1"],
   ["lt", "Lithuanian (Lietuvių)", "_LANGID(LANG_LITHUANIAN)"],
   ["lv", "Latvian (latviešu valoda)", "_LANGID(LANG_LATVIAN)"],
@@ -70,46 +54,27 @@ const gLangs: string[][] = [
   ["my", "Malaysian (Bahasa Melayu)", "_LANGID(LANG_MALAY)"],
   ["ne", "Nepali (नेपाली)", "_LANGID(LANG_NEPALI)"],
   ["nl", "Dutch (Nederlands)", "_LANGID(LANG_DUTCH)"],
-  [
-    "nn",
-    "Norwegian Neo-Norwegian (Norsk nynorsk)",
-    "MAKELANGID(LANG_NORWEGIAN, SUBLANG_NORWEGIAN_NYNORSK)",
-  ],
-  [
-    "no",
-    "Norwegian (Norsk)",
-    "MAKELANGID(LANG_NORWEGIAN, SUBLANG_NORWEGIAN_BOKMAL)",
-  ],
+  ["nn", "Norwegian Neo-Norwegian (Norsk nynorsk)", "MAKELANGID(LANG_NORWEGIAN, SUBLANG_NORWEGIAN_NYNORSK)"],
+  ["no", "Norwegian (Norsk)", "MAKELANGID(LANG_NORWEGIAN, SUBLANG_NORWEGIAN_BOKMAL)"],
   ["pa", "Punjabi (ਪੰਜਾਬੀ)", "_LANGID(LANG_PUNJABI)"],
   ["pl", "Polish (Polski)", "_LANGID(LANG_POLISH)"],
   ["pt", "Portuguese - Portugal (Português)", "_LANGID(LANG_PORTUGUESE)"],
   ["ro", "Romanian (Română)", "_LANGID(LANG_ROMANIAN)"],
   ["ru", "Russian (Русский)", "_LANGID(LANG_RUSSIAN)"],
+  ["sat", "Santali (ᱥᱟᱱᱛᱟᱲᱤ)", "(LANGID)-1"],
   ["si", "Sinhala (සිංහල)", "_LANGID(LANG_SINHALESE)"],
   ["sk", "Slovak (Slovenčina)", "_LANGID(LANG_SLOVAK)"],
   ["sl", "Slovenian (Slovenščina)", "_LANGID(LANG_SLOVENIAN)"],
   ["sn", "Shona (Shona)", "(LANGID)-1"],
-  [
-    "sp-rs",
-    "Serbian (Latin)",
-    "MAKELANGID(LANG_SERBIAN, SUBLANG_SERBIAN_LATIN)",
-  ],
+  ["sp-rs", "Serbian (Latin)", "MAKELANGID(LANG_SERBIAN, SUBLANG_SERBIAN_LATIN)"],
   ["sq", "Albanian (Shqip)", "_LANGID(LANG_ALBANIAN)"],
-  [
-    "sr-rs",
-    "Serbian (Cyrillic)",
-    "MAKELANGID(LANG_SERBIAN, SUBLANG_SERBIAN_CYRILLIC)",
-  ],
+  ["sr-rs", "Serbian (Cyrillic)", "MAKELANGID(LANG_SERBIAN, SUBLANG_SERBIAN_CYRILLIC)"],
   ["sv", "Swedish (Svenska)", "_LANGID(LANG_SWEDISH)"],
   ["ta", "Tamil (தமிழ்)", "_LANGID(LANG_TAMIL)"],
   ["th", "Thai (ภาษาไทย)", "_LANGID(LANG_THAI)"],
   ["tl", "Tagalog (Tagalog)", "_LANGID(LANG_FILIPINO)"],
   ["tr", "Turkish (Türkçe)", "_LANGID(LANG_TURKISH)"],
-  [
-    "tw",
-    "Chinese Traditional (繁體中文)",
-    "MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_TRADITIONAL)",
-  ],
+  ["tw", "Chinese Traditional (繁體中文)", "MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_TRADITIONAL)"],
   ["uk", "Ukrainian (Українська)", "_LANGID(LANG_UKRAINIAN)"],
   ["uz", "Uzbek (O'zbek)", "_LANGID(LANG_UZBEK)"],
   ["vn", "Vietnamese (Việt Nam)", "_LANGID(LANG_VIETNAMESE)"],
@@ -226,19 +191,13 @@ function genTranslationInfoCpp() {
 
   console.log(`langs: ${langs.length}, gLangs: ${gLangs.length}`);
 
-  const langcodes = langs
-    .map((lang) => `  ${cEscapeForCompact(lang.code)}`)
-    .join(" \\\n");
+  const langcodes = langs.map((lang) => `  ${cEscapeForCompact(lang.code)}`).join(" \\\n");
   console.log(`langcodes: ${langcodes.length} bytes`);
 
-  const langnames = langs
-    .map((lang) => `  ${cEscapeForCompact(lang.name)}`)
-    .join(" \\\n");
+  const langnames = langs.map((lang) => `  ${cEscapeForCompact(lang.name)}`).join(" \\\n");
   console.log(`langnames: ${langnames.length} bytes`);
 
-  const langids = langs
-    .map((lang) => `  ${lang.msLangID}`)
-    .join(",\n");
+  const langids = langs.map((lang) => `  ${lang.msLangID}`).join(",\n");
   console.log(`langids: ${langids.length} bytes`);
 
   const rtlInfo: string[] = [];
